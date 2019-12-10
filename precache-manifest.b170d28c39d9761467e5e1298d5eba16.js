a76acb87ddd742b4a1bee8bf77c35205761c1f65
john mogi <john@accessibe.com>
2019-12-10 03:45:31 +0200

    Updates

---
+++ precache-manifest.b170d28c39d9761467e5e1298d5eba16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881a1d1206715c4e4f535eb9735aef3a",
+    "url": "/puppies_react/index.html"
+  },
+  {
+    "revision": "4c66d218c7675b4bd45b",
+    "url": "/puppies_react/static/css/main.a8b55214.chunk.css"
+  },
+  {
+    "revision": "476930832fbc2bd4db94",
+    "url": "/puppies_react/static/js/2.34333c27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/puppies_react/static/js/2.34333c27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c66d218c7675b4bd45b",
+    "url": "/puppies_react/static/js/main.2c33c515.chunk.js"
+  },
+  {
+    "revision": "fc20f326b8629b17e798",
+    "url": "/puppies_react/static/js/runtime-main.b7b9b0e1.js"
+  },
+  {
+    "revision": "c0f3b33bdd911a3c6e0c15829e3545f6",
+    "url": "/puppies_react/static/media/how-to-react-to-a-dog-biting_large.c0f3b33b.webp"
+  }
+]);
